51f9f4caa7e48f70e86d2a657b1374d3b990f9bb
we <2333@qq.com>
2018-05-05 17:14:38 +0800

    routine
    routine

--- core/miniwork_lwj/miniwork_lwj/def.h
+++ core/miniwork_lwj/miniwork_lwj/def.h
@@ -1,20 +1,27 @@
 #pragma once
 #include<iostream>
-class cursor
+class cursor//逻辑上的光标，不是实际的那个，用于在链表中定位
 {
 public:
 	cursor();
 	~cursor();
-
+	unsigned int getRow(void) { return this->row; }
+	unsigned int getCol(void) { return this->col; }
+	void setRow(unsigned n) { this->row = n; }
+	void setCol(unsigned n) { this->col = n; }
 private:
-	char * ch;//指向字符,光标前的字
+	Qchar * ch;//指向字符,光标前的字
 	linehead * liH;//指向行头
 	block * bl;//指向块
-			   //暂定,界面绘制参数 
+			   //暂定,界面绘制参数
+	unsigned int row;//从1开始计算
+	unsigned int col;//从1开始计算 
 };
 
 cursor::cursor()
 {
+	row = 1;
+	col = 1;
 }
 
 cursor::~cursor()
@@ -25,10 +32,11 @@ class block//字符块
 public:
 	block();
 	~block();
-
-
+	block* getNext(void) { return next; }
+	block* setNext(block* np) { next = np; }
+	int getSize(void) { return textSize; }
 private:
-	char ch[101];//字符串,无效字符\0
+	Qchar ch[101];//字符串,无效字符\0
 	int head, tail;//串头,串尾
 	int textSize;//串长
 	block * next;//下一block
@@ -37,9 +45,11 @@ private:
 
 block::block()
 {
-	head = NULL; tail = NULL;
+	ch[0] = '\0'; //ch={""};这个不行
+	head = 0;
+	tail = 0;
 	textSize = 0;
-	next = NULL;
+	next = nullptr;
 }
 
 block::~block()
@@ -51,32 +61,35 @@ class linehead//行头结点
 public:
 	linehead();
 	~linehead();
-
 	block* newBlock();//仅申请new block 
 	bool deleteBlock(block* target);
-	friend class temText;
+	linehead * getNext(void) { return this->next; }
+	void setNext(linehead* np) { this->next = np; }
+	bool QcharInsert(unsigned int col, Qchar c);//根据光标插入字符，并修改光标
+	bool stringInsert();//TODO: 插入字符串，等待数据类型确定D
 private:
-	block * firstBlock;//指向第一个block
+	int num;//TODO: 如果没有用就删掉
 	linehead * next;//下一个行头
+	block * firstBlock;//指向第一个block
 
 };
 
 linehead::linehead()
 {
-	this->firstBlock = NULL;
-	this->next = NULL;
+	this->firstBlock = nullptr;
+	this->next = nullptr;
 }
 block* linehead::newBlock() {//在光标所指的block之后插入新的block
 	block* tem = new block;
 	return tem;
 }
-bool linehead::deleteBlock(block* target) {//仅仅删除block,没考虑前后交替的
+bool linehead::deleteBlock(block* target) {
 	if (!target)
 		return false;
 	delete target;
 	return true;
 }
-linehead::~linehead()//释放当前行后所有的block
+linehead::~linehead()//释放当前行后的blocks
 {
 	block* tem = this->firstBlock;
 	block* nextblk;
@@ -89,31 +102,24 @@ linehead::~linehead()//释放当前行后所有的block
 
 class temText//文字编辑内存
 {
-	public:
-		temText();
-		~temText();
-
-	private:
-		int parcounter;//段计数
-		linehead * head;//行头链表头结点
-		bool changeMode;//换行模式,true为非自动变化
+public:
+	temText();
+	~temText();
+	linehead * curToRow(cursor cur);//根据光标返回行结点
+	bool QcharInsert(cursor &cur, Qchar c);//根据光标插入字符，并修改光标
+	bool stringInsert();//TODO: 插入字符串，等待数据类型确定D
+	bool QcharDel(cursor &cur, bool isFoward);//删除字符，判断前后
+private:
+	int parcounter;//段计数
+	linehead * head;//行头链表头结点,无内容
+	bool changeMode;//换行模式,true为非自动变化
 
 };
 
 temText::temText()
 {
 	parcounter = 0;
-	head = NULL;
+	head = new linehead;
+	head->setNext(new linehead);
 	changeMode = true;
-}
-
-temText::~temText()
-{
-	linehead * tem = head->next;
-	linehead * nexthd;
-	while (!tem) {
-		nexthd = tem->next;
-		delete tem;
-		tem = nexthd;
-	}
 }
--- gui/mainwindow.h
+++ gui/mainwindow.h
@@ -14,9 +14,21 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void newFile();   // 新建操作
+    bool maybeSave(); // 判断是否需要保存
+    bool save();      // 保存操作
+    bool saveAs();    // 另存为操作
+    bool saveFile(const QString &fileName); // 保存文件
+
 
 private:
     Ui::MainWindow *ui;
+    // 为真表示文件没有保存过，为假表示文件已经被保存过了
+    bool isUntitled;
+    // 保存当前文件的路径
+    QString curFile;
 };
+
+
 
 #endif // MAINWINDOW_H
