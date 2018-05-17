db898ca2fe683109622536aa13d3b837edaa776a
we <2333@qq.com>
2018-05-17 19:33:09 +0800

    根据新的数据结构修改了文件操作,修改了arrayline

---
+++ fileOperate.h
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <fstream>
+
+#include <string>
+using namespace std;
+
+
+#include "arrayline.h"
+#include <QFileDialog>
+bool vacantBody(temText & body){//TODO:清空body
+
+}
+
+void newfile(const char* & addr,temText & body){
+   if(addr[0]){
+       // TODO:询问是否保存当前文件并新建,
+        //若否，直接返回；
+        {
+        }
+        //若是，保存文件
+        {
+            savefile(addr,body);
+            addr[0]="\0";
+        }
+   }//清空body，还原cur位置
+    vacantBody(body);
+    body.cur=NULL;
+}
+
+bool openfile(const char* & addr,cursor & cur){//XXX:打开文件的操作:系统化文件夹窗口里选取
+    //TODO:如果已经打开了一份文件还想打开第二份
+    if(addr[0]){//已经打开过一份
+        //TODO:询问是否保存并重新打开,
+        // 若否，直接返回；若是，保存并清空body，还原cur位置；
+    }//否则直接打开
+    QString filename = QFileDialog::getOpenFileName(//得到文件路径
+                        this,
+                        "Open Document",
+                        QDir::currentPath(),//打开程序地址下的目录
+                        "Text files (*.txt )");//file filter,只能打开txt
+
+    if (!filename.isNull()) { //用户选择了文件
+        QByteArray ba=filename.toLatin1();  
+        addr=ba.data();//add存储的是绝对地址 TOTEST:（函数内部的const是全局不变的吗？）
+        
+        ifstream buf(addr);
+        //FIXME:偷懒,没有检查打开失败的情况 (●'◡'●)   
+        string media;
+		while (getline(buf,media))
+			cur.insertStr(media);//TODO:将string插入到linehead里
+        return true; 
+    } else // 用户取消选择
+        return false;
+}
+//TODO: 退出之前询问是否保存
+bool savefile(const char* & addr,cursor & cur){
+    if(!addr[0]){//TODO:新建的文件无原地址，应该有什么打开系统文件夹的操作
+
+    }else{//否则直接保存到ch
+        ofstream buf(addr);
+        //FIXME:偷懒,没有检查打开失败的情况 (●'◡'●)   
+        linehead * tem=cur.getFirstLine();
+        while(tem!=nullptr){
+            buf<<tem->chs<<std::endl;
+            tem=tem->getNext();
+        }
+		return true;
+    }
+}
+
+
+
