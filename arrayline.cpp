5104c4788581f822533f7fe39aa670edbdb7ce4e
EDLMM <37733136+EDLMM@users.noreply.github.com>
2018-05-19 17:07:52 +0800

    arrayline.h-version-alpha 1.1
    1.所有的基本操作已经完成（主要是insert）
    2.cursor::delBlock()和cursor::copyBlock()有微量的修改，之前逻辑不顺畅的地方，主要是(r2,c2)的位置往后移动了一位
    3.cursor::insertStr() 可以用于读取新文件的插入，也可以在文本中间的某一坐标处插入块选的字符串
    4.delC（即backspace和delete单个字符串）和上述的修改适配；除了在行内删除以外，还有在行头(x,1)进行backspace和在行尾(x,nowLine->getsize()+1)进行delete操作时，将两行整合在一起的功能
    5.ps之前就内置了debug函数，移动坐标后，使用printNL和printFULL可以输出当前行和全文。

--- arrayline.cpp
+++ arrayline.cpp
@@ -6,18 +6,49 @@ int main(void)
     cursor* cur=new cursor;	
 
     // std::cout<<"--------debug "<<2<<" -------"<<std::endl;//FIXME:
-    std::string tx="ojbkkkkkkkkkkkkkkk\npierlewio\nhehe\ncdcdcd";
-    cur->insertStr(tx);//row=1,col=1
-
-    std::cout<<"fulltext:"<<std::endl;
-    cur->setAxis(1,1);
-    cur->printFULL();
     
-    cur->delFULL();
+    char* bigstr=(char*)malloc(sizeof(char)*150);
+
+    std::string tx="ojbkkk\npierlewio\nhehe\ncdcdcd";
+
+    // memset(bigstr,'a',sizeof(char)*146);
+    // bigstr[0]='\n';
+    // tx.append(bigstr);
+    
+    cur->insertStr(tx);//row=1,col=1
+    cur->getAxis();
+    
     std::cout<<"fulltext:"<<std::endl;
     cur->setAxis(1,1);
     cur->printFULL();
 
-    int quit;
-    std::cin>>quit;
+    // cur->setAxis(1,1);
+    // cur->delBlock(4,3);
+    // cur->getAxis();
+
+    // std::cout<<"fulltext:"<<std::endl;
+    // cur->setAxis(1,1);
+    // cur->printFULL();
+    
+    // cur->setAxis(2,5);
+    // cur->delBlock(5,140);
+
+    // std::cout<<"fulltext after delB:"<<std::endl;
+    // cur->setAxis(1,1);
+    // cur->printFULL();
+
+    std::cout<<std::endl<<"delC() test:"<<std::endl;
+    cur->setAxis(1,1);
+    cur->delC(1);
+    cur->setAxis(4,7);
+    cur->delC(-1);
+
+    // cur->setAxis(2,1);cur->delC(1);
+    cur->setAxis(1,7);cur->delC(-1);
+    std::cout<<"delC fulltext:"<<std::endl;
+    cur->setAxis(1,1);
+    cur->printFULL();
+
+    // int quit;
+    // std::cin>>quit;
 }
