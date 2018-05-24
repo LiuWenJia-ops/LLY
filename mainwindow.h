74fcc6def2e745232f2c26dbace6bb5707aafaa7
Eveylens <37871270+Eveylens@users.noreply.github.com>
2018-05-24 18:52:03 +0800

    new

--- mainwindow.h
+++ mainwindow.h
@@ -5,6 +5,10 @@
 #include <QObject>
 #include <QTextEdit>
 #include <QTextCursor>
+#include <QKeyEvent>
+#include <QTextBlock>
+
+#include <QMainWindow>
 
 namespace Ui {
 class MainWindow;
@@ -13,18 +17,49 @@ class MainWindow;
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+
+    void on_textEdit_textChanged();
+
+    void on_actionopen_triggered();
+
+    void on_actionnew_triggered();
+
+    void on_actionsave_triggered();
+
 protected:
     bool eventFilter(QObject *obj, QEvent *event);
-
+    void flush();//
+    void setTwoEnd(int & r,int & c);//
 
 private:
     Ui::MainWindow *ui;
     QTextEdit *textEdit;
     QTextCursor tcursor;
+};
+
+//只有在派生类中才可以通过派生类对象访问基类的protected成员。
+//可以被该类中的函数、子类的函数、以及其友元函数访问,但不能被该类的对象访问
+class MyEvent : private QTextEdit
+{
+public:
+    void keyPressEvent(QKeyEvent *e){
+        QTextEdit::keyPressEvent(e);
+    }
+    void mousePressEvent(QMouseEvent *e){
+        QTextEdit::mousePressEvent(e);
+    }
+    void mouseMoveEvent(QMouseEvent *e){
+        QTextEdit::mouseMoveEvent(e);
+    }
+    void mouseReleaseEvent(QMouseEvent *e){
+        QTextEdit::mouseReleaseEvent(e);
+    }
 };
 
 #endif // MAINWINDOW_H
