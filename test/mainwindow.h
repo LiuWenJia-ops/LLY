780c9a96bf44c671bc811a2b17f5ad4075f471e7
EDLMM <37733136+EDLMM@users.noreply.github.com>
2018-05-27 14:02:55 +0800

    Revert "合并"
    This reverts commit f4a0f457bd3abfbdff46a1a862fae8afaa13aefc.

--- test/mainwindow.h
+++ test/mainwindow.h
@@ -3,12 +3,6 @@
 
 #include <QMainWindow>
 
-#include <QObject>
-#include <QTextEdit>
-#include <QTextCursor>
-#include <QKeyEvent>
-#include <QTextBlock>
-
 namespace Ui {
 class MainWindow;
 }
@@ -31,34 +25,8 @@ private slots:
 
     void on_actionsave_triggered();
 
-protected:
-    bool eventFilter(QObject *obj, QEvent *event);
-    void flush();//
-    void setTwoEnd(int & r,int & c);//
-
 private:
     Ui::MainWindow *ui;
-    QTextEdit *textEdit;
-    QTextCursor tcursor;
-};
-
-//只有在派生类中才可以通过派生类对象访问基类的protected成员。
-//可以被该类中的函数、子类的函数、以及其友元函数访问,但不能被该类的对象访问
-class MyEvent : private QTextEdit
-{
-public:
-    void keyPressEvent(QKeyEvent *e){
-        QTextEdit::keyPressEvent(e);
-    }
-    void mousePressEvent(QMouseEvent *e){
-        QTextEdit::mousePressEvent(e);
-    }
-    void mouseMoveEvent(QMouseEvent *e){
-        QTextEdit::mouseMoveEvent(e);
-    }
-    void mouseReleaseEvent(QMouseEvent *e){
-        QTextEdit::mouseReleaseEvent(e);
-    }
 };
 
 #endif // MAINWINDOW_H
